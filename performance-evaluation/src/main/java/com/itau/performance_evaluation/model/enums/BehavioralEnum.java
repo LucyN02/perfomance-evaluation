0e247dd4d29a903dde91b8c3cd9222495cac6bee
Lucy Neves <luciananbrito22@gmail.com>
2025-10-31 10:30:44 -0300

    fix: enum

--- performance-evaluation/src/main/java/com/itau/performance_evaluation/model/enums/BehavioralEnum.java
+++ performance-evaluation/src/main/java/com/itau/performance_evaluation/model/enums/BehavioralEnum.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public enum BehavioralEnum {
-    COLLAB("PROMOVE_COLABORACAO", "Você promove um ambiente colaborativo?", 4),
-    LEARN("APRENDE_CONTINUAMENTE", "Você se atualiza e aprende o tempo todo?", 3),
-    DATA("USA_DADOS_DECISOES", "Você utiliza dados para tomar suas decisões?", 5),
-    AUTONOMY("TRABALHA_AUTONOMIA", "Você trabalha com autonomia?", 3);
+    COLLAB("PROMOVE_COLABORACAO", "Você promove um ambiente colaborativo?", 3),
+    LEARN("APRENDE_CONTINUAMENTE", "Você se atualiza e aprende o tempo todo?", 2),
+    DATA("USA_DADOS_DECISOES", "Você utiliza dados para tomar suas decisões?", 3),
+    AUTONOMY("TRABALHA_AUTONOMIA", "Você trabalha com autonomia?", 2);
 
     private final String resume;
     private final String description;
